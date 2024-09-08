78bf864faa43523d86e9fdae1013b47e056c8eab
NTkernel <ntkernel1@gmail.com>
2024-09-08 14:02:58 +0100

    1.0

---
+++ Plugin.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using UnityEngine;
+using System.Collections;
+
+[BepInPlugin("com.ntkernel.GorillaTag.DeepFryCamera", "DeepFry Camera", "1.0.0")]
+public class DisableDeepFryCamera : BaseUnityPlugin
+{
+    private void Start()
+    {
+       
+        StartCoroutine(DeepFryCameraIn(2f));
+    }
+
+    private IEnumerator DeepFryCameraIn(float delay)
+    {
+        
+        yield return new WaitForSeconds(delay);
+
+        
+        var deepFryCamera = GameObject.Find("Third Person Camera");
+
+        if (deepFryCamera != null)
+        {
+           
+            deepFryCamera.SetActive(false);
+            Logger.LogInfo("Deepfry gone >:(.");
+        }
+        else
+        {
+            Logger.LogError("Couldnt find Deep Fry target.");
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace FPC
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.ntkernel.gorillatag.DeepFryCamera";
+        public const string Name = "DeepFryCamera";
+        public const string Version = "1.0.0";
+    }
+}
